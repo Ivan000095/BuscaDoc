707f06418bddb6e01035097794d9a0a91bbb5979
Mar-078 <margaritaramirez0077@gmail.com>
2025-08-10 17:47:15 -0600

    Update edit.php

--- vistas/Establecimientos/edit.php
+++ vistas/Establecimientos/edit.php
@@ -1,7 +1,7 @@
 <?php 
-    require('../../modelos/Establecimiento.php');
-    $Id_Establecimiento = $_REQUEST['Id_Establecimiento'];
-    $establecimiento = Establecimiento :: find($Id_Establecimiento);
+    require('../../modelos/Farmacia.php');
+    $Id_Farmacia = $_REQUEST['Id_Farmacia'];
+    $Farmacia = Farmacia :: find($Id_Farmacia);
 
 ?>
 
@@ -19,40 +19,40 @@
            
         <div id="contenido"> 
         <br>    
-        <h1 id="titulo">Editar Establecimiento</h1>
+        <h1 id="titulo">Editar Farmacia</h1>
         <br>
-        <form action="update.php?Id_Establecimiento=<?php echo $establecimiento -> Id_Establecimiento;?>" method="POST">
+        <form action="update.php?Id_Farmacia=<?php echo $Farmacia -> Id_Farmacia;?>" method="POST">
         <div class="row justify-content-center">
             <div class="col-4">
                 <label for="Nombre" class="form-label">Nombre</label>
-                <input type="text" name="Nombre" id="Nombre" class="form-control" value="<?php echo $establecimiento -> Nombre; ?>">
+                <input type="text" name="Nombre" id="Nombre" class="form-control" value="<?php echo $Farmacia -> Nombre; ?>">
             </div>
         </div>
 
           <div class="row justify-content-center">
             <div class="col-4">
                 <label for="Descripcion" class="form-label">Descripcion</label>
-                <input type="text" name="Descripcion" id="Descripcion" class="form-control" value="<?php echo $establecimiento -> Descripcion; ?>">
+                <input type="text" name="Descripcion" id="Descripcion" class="form-control" value="<?php echo $Farmacia -> Descripcion; ?>">
             </div>
         </div>
 
           <div class="row justify-content-center">
             <div class="col-4">
                 <label for="Horario" class="form-label">Horario</label>
-                <input type="text" name="Horario" id="Horario" class="form-control" value="<?php echo $establecimiento -> Horario; ?>">
+                <input type="text" name="Horario" id="Horario" class="form-control" value="<?php echo $Farmacia -> Horario; ?>">
             </div>
         </div>
 
          <div class="row justify-content-center">
             <div class="col-4">
                 <label for="Dias_labo" class="form-label">Dias Laborales</label>
-                <input type="text" name="Dias_labo" id="Dias_labo" class="form-control" value="<?php echo $establecimiento -> Dias_labo; ?>">
+                <input type="text" name="Dias_labo" id="Dias_labo" class="form-control" value="<?php echo $Farmacia -> Dias_labo; ?>">
             </div>
         </div>        
           <div class="row justify-content-center">
             <div class="col-4">
                 <label for="DireccionyRef" class="form-label">Direccion y Referencia</label>
-                <input type="text" name="DireccionyRef" id="DireccionyRef" class="form-control" value="<?php echo $establecimiento -> DireccionyRef; ?>">
+                <input type="text" name="DireccionyRef" id="DireccionyRef" class="form-control" value="<?php echo $Farmacia -> DireccionyRef; ?>">
             </div>
         </div>
         
