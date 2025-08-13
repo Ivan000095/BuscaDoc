e2292305686ab646aa71abb80040e0c26d38aa44
Ivan000095 <ivanlanestosa8@gmail.com>
2025-08-13 13:00:42 -0600

    Delete comentar.php

--- comentar.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require('modelos/comentario.php');
-    $comentario = new Comentario();
-    $comentario->id = $_REQUEST['id'];
-    $comentario->comentario = $_REQUEST['comentario'];
-    $comentario->savecom();
-    echo '<meta http-equiv="refresh" content="0;url=/BuscaDoc/comentarios.php">';
-?>
