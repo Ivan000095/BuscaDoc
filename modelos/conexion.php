60f14c69e653f620cf8fa64b53dac52ccbd64b11
Ivan000095 <ivanlanestosa8@gmail.com>
2025-08-13 12:58:33 -0600

    Delete modelos directory

--- modelos/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-    $con = new mysqli("127.0.0.1", "root", "", "buscadoc");
-    if ($con->connect_errno) {
-        die("Fallo al conectar a MySQL: (" . 
-            $con->connect_errno . ") " . 
-            $con->connect_error);
-    }
-?>
